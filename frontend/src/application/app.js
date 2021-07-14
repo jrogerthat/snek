db8ee9a744581428311c9cdaaf7cc5d01bcadc92
Jen Rogers <jen.rogers@jenroge-ltmyy46.internal.salesforce.com>
2021-07-14 14:30:14 -0600

    save

--- frontend/src/application/app.js
+++ frontend/src/application/app.js
@@ -1,12 +1,10 @@
 import "../styles/index.scss";
 import * as d3 from "d3";
-import * as d3Array from "d3-array";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "../components/sidebar";
 import * as dataF from "../../vendors/artifact-data.json";
 import * as depend from "../components/dependencyVis";
-
-const radius = 9;
+import { dropDownInteraction } from "./generalHelpers";
 
 let nodeFormat = dataF.map((m, i)=>{
   m.name = m['Artifact Type'];
@@ -15,7 +13,7 @@ let nodeFormat = dataF.map((m, i)=>{
   return m});
 
 let svg = d3.select('#container').select('#wrapper').append('svg').classed('svg-wrap', true);
-
+dropDownInteraction(nodeFormat);
 depend.renderDependencyVis(nodeFormat);
 
 
--- frontend/src/application/generalHelpers.js
+++ frontend/src/application/generalHelpers.js
@@ -1,8 +1,23 @@
-
+import * as d3 from "d3";
+import { renderDependencyVis } from "../components/dependencyVis";
 
 export function machineOrHuman(d){
     if(d['Transmission Mode'] === "Boundry Crossing/M-H" || d['Transmission Mode'] === "Non-Boundary/M-M"){ return "machine"}
     else if(d['Transmission Mode'] === "Boundry Crossing/H-M" || d['Transmission Mode'] === "Non-Boundary/H-H" ){
       return "human"
     }
+  }
+
+  const dropdownDictionary = {
+      'Human vs Machine' : ()=> console.log('Human vs Machine'),
+      'Artifact Dependencies' : renderDependencyVis
+  }
+
+  export function dropDownInteraction(nodes){
+      //console.log('drop down loaded', d3.select('.dropdown').selectAll('.dropdown-content').selectAll('p'))
+      d3.select('.dropdown').selectAll('.dropdown-content').selectAll('p').on('click', (event, d)=>{
+        d3.select('.dropdown').select('span').text(d3.select(event.target).text());
+        d3.select('#wrapper').select('svg').selectAll('*').remove();
+        dropdownDictionary[d3.select(event.target).text()](nodes);
+      });
   }
