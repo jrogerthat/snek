8bb873a64833794b61219a0aac6e26a48884eb95
Jen Rogers <jen.rogers@jenroge-ltmyy46.internal.salesforce.com>
2021-07-29 21:50:48 -0600

    add class

--- frontend/src/application/generalHelpers.js
+++ frontend/src/application/generalHelpers.js
@@ -3,9 +3,12 @@ import { renderDependencyVis } from "../components/dependencyVis";
 import { renderHumanVsMachine } from "../components/humanVsMachineVis";
 
 export function machineOrHuman(d){
-    if(d['Transmission Mode'] === "Boundry Crossing/M-H" || d['Transmission Mode'] === "Non-Boundary/M-M"){ return "machine"}
-    else if(d['Transmission Mode'] === "Boundry Crossing/H-M" || d['Transmission Mode'] === "Non-Boundary/H-H" ){
-      return "human"
+    if(d['Transmission Mode'] === "Boundry Crossing/M-H" || d['Transmission Mode'] === "Non-Boundary/M-M"){ 
+      return "machine";
+    }else if(d['Transmission Mode'] === "Boundry Crossing/H-M" || d['Transmission Mode'] === "Non-Boundary/H-H" ){
+      return "human";
+    }else{
+      return "other";
     }
   }
 
